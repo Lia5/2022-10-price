3988f93e2906950d50e6fa5a06d13d16724234fb
lia5 <liaforweb@gmail.com>
2022-10-15 16:05:26 +0300

    init, price: desctop, mob

---
+++ gulpfile.js
@@ -0,0 +1,155 @@
+//NPM-MODULES
+var gulp       	 = require('gulp');
+var sass       	 = require('gulp-sass');
+var browserSync 	 = require('browser-sync').create();
+var autoprefixer 	 = require('gulp-autoprefixer');
+var plumber 		 = require('gulp-plumber');
+var babel         = require('gulp-babel'),
+	imagemin      = require('gulp-imagemin'),
+	concat        = require('gulp-concat'),
+	uglify        = require('gulp-uglify'),
+	sourcemaps    = require('gulp-sourcemaps'),
+	cleancss      = require('gulp-clean-css'),
+	// gcmq      = require('gulp-group-css-media-queries'),
+	rename        = require('gulp-rename'),
+	notify        = require("gulp-notify"),
+	include       = require('gulp-include'),
+	util          = require('gulp-util'),
+	eslint        = require('gulp-eslint'),
+	gulpIf        = require('gulp-if');
+gulp.task('browser-sync', function(done) { 
+  browserSync.init({
+    server: {
+      baseDir: './dist'
+    },
+    notify: false
+  });
+  
+  browserSync.watch('dist/').on('change', browserSync.reload);
+  
+  done()
+});	
+
+gulp.task('styles', function(done){
+  gulp.src('src/scss/**/*.scss')
+    .pipe(plumber({
+      errorHandler : function(err) {
+        console.log(err);
+        this.emit('end');
+      }
+    }))
+    .pipe(sass({errLogToConsole: true}))
+
+	.pipe(sourcemaps.init({loadMaps: true}) )
+	.pipe(sass({ outputStyle: 'expanded' }).on("error", notify.onError()))
+	.pipe(rename({ suffix: '.min', prefix : '' }))
+    .pipe(autoprefixer({
+      browsers: ['last 5 versions'],
+      cascade: false
+    }))
+	// .pipe(gcmq())
+	// .pipe(cleancss( {level: { 1: { specialComments: 0 } } })) // Opt., comment out when debugging
+	.pipe(sourcemaps.write('map',{addComment: false}))
+	.pipe(gulp.dest('dist/css'))
+	.pipe(browserSync.reload({stream: true}));
+  
+  done()
+});
+
+const isFixed = (file) => (file.eslint != null && file.eslint.fixed);
+
+gulp.task('jsLint', function() {
+    const lintType = 1;
+    return gulp.src(['./src/js/**/**.js', '!./src/js/min/**.js'], {since: gulp.lastRun('jsLint')})
+        .pipe(eslint({
+            parserOptions: {
+                "ecmaVersion": 6
+            },
+            fix: true,
+            rules: {
+                'quotes': [1, 'single'],
+                'semi': [1, 'always'],
+                'no-console': [1, {allow: ['error']}],
+                'no-empty': [lintType, {'allowEmptyCatch': true}],
+                'no-debugger': [lintType],
+                'no-alert': [lintType],
+                'camelcase': [1, {properties: "never"}],
+                'indent': [
+                    1,
+                    4,
+                    {'SwitchCase': 1}
+                ],
+                'no-multiple-empty-lines': [1, {max: 2, maxBOF: 1}],
+                'lines-around-comment': [1, {'beforeBlockComment': true}]
+            },
+        }))
+        .pipe(eslint.format())
+        .pipe(gulpIf(isFixed, gulp.dest('dist/js')))
+        .pipe(eslint.failAfterError())
+});
+
+gulp.task('js', function() {
+	return gulp.src([
+		'./src/libs/1.jquery/dist/jquery.min.js',
+		'./src/js/custom.js',
+		])
+    .pipe(plumber())
+	.pipe(concat('scripts.min.js'))
+	// .pipe(uglify()) // Mifify js (opt.)
+	.pipe(include())
+	.pipe(sourcemaps.init({loadMaps: true}))
+	.pipe(babel({
+		presets: ['@babel/preset-env']
+	}))
+	.pipe(rename(function (path) {
+		// path.dirname += "js";
+		path.extname = ".js";
+	}))
+	.pipe(uglify())
+	.pipe(sourcemaps.write('map',{addComment: false}) )
+	.pipe(gulp.dest('dist/js'))
+	.pipe(browserSync.reload({stream: true}));
+});
+
+// gulp.task('js2', function() {
+// 	return gulp.src([
+// 		'src/js/custom.js', // Always at the end
+// 		])
+// 	.pipe(plumber())
+// 	.pipe(concat('custom.js'))
+// 	// .pipe(uglify()) // Mifify js (opt.)
+// 	.pipe(gulp.dest('dist/js'))
+// 	.pipe(browsersync.reload({ stream: true }))
+// });
+
+gulp.task('images', function() {
+	return gulp.src(['src/img/**/*.{gif,jpg,png,svg,webp}'])
+	.pipe(plumber())
+	.pipe(imagemin({progressive: true}))
+	.pipe(gulp.dest("dist/img"))
+	.pipe(browserSync.reload({stream: true}));
+});
+
+gulp.task('fonts', function() {
+  return gulp.src('src/fonts/**/*.{eot,ttf,woff,svg,woff2}')
+	.pipe(gulp.dest('dist/fonts'))
+	.pipe(browserSync.reload({stream: true}));
+});
+gulp.task('html', function() {
+  return gulp.src(['src/**/*.html', '!src/libs/**/*.html'])
+	.pipe(gulp.dest('dist/'))
+	.pipe(browserSync.reload({stream: true}));
+});
+
+gulp.task('watch', gulp.series('styles', 'html', 'js', 'images', 'fonts', 'browser-sync', function(done) {
+	gulp.watch('src/**/*.scss', gulp.series('styles'));
+	gulp.watch(['src/js/**/*.js'], gulp.series('jsLint', 'js'));
+	// gulp.watch(['src/**/*.js'], gulp.series('js'));
+	gulp.watch('src/img/**/*.{gif,jpg,png,svg}', gulp.series('images'));
+	gulp.watch(['src/*.html', '!src/libs/**/*.html'], gulp.series('html'));
+	gulp.watch('src/fonts/**/*.{eot,ttf,woff,svg,woff2}', gulp.series('fonts'));
+
+  done()
+}));
+
+gulp.task('default', gulp.series('watch'));
---
+++ src/js/custom.js
@@ -0,0 +1,139 @@
+const nav = () => {
+    const nav = document.querySelector(".js-nav");
+    const navLinks = nav.querySelectorAll(".nav__item");
+    const slideRect = nav.querySelector(".nav__slider-rect");
+    const prices = document.querySelectorAll('.card__price--type');
+    let priceType; 
+
+    nav.addEventListener("click", (evt) => {
+       if (!evt.target.classList.contains("nav__item")) {
+          return;
+       }
+       evt.preventDefault();
+ 
+       navLinks.forEach((item, index) => {
+          item.classList.remove("active");
+          console.log(index);
+       });
+ 
+ 
+       if (!evt.target.classList.contains("active")) {
+          evt.target.classList.add("active");
+          priceType = evt.target.getAttribute('data-type'); // Получаем имя таба, который нам нужен
+          console.log(priceType);
+          selectTabContent(priceType); // Запускаем функцию, чтобы показать выбранный таб
+       }
+       function selectTabContent(tab) {
+         // Проходим по всем табам и проверяем есть ли у элемента класс, равный имени таба(переменной priceType). Если есть, то добавляем класс активного таба, если нет, то удаляем этот класс
+         prices.forEach((item) => {
+             let classList = item.classList;
+             classList.contains(tab) ? classList.add('active') : classList.remove('active');
+         });
+      }
+
+       slideRect.style.transform = `translateX(${evt.target.dataset.transform}%)`;
+    });
+ };
+ nav();
+
+
+
+
+
+// $(function() {
+//     $('.js-preloader').preloadinator({
+//         minTime: 2000,
+//         afterRemovePreloader: function() {
+//             $(this).remove();
+//             $('body').removeClass('body-modal-open');
+//         }
+//     });
+//     //menu
+//     if(jQuery('.menu-toggle').length) {
+//         var menu = $('.menu-toggle');
+//         menu.on('click', function(){
+//             $('.main-menu').toggleClass('active');
+//             $('.menu-toggle').toggleClass('active');
+//             $('body').toggleClass('body-modal-open');
+//         });
+//         $('.main-menu').mouseup(function (e){ // событие клика по веб-документу
+//             var div = $(".main-menu ul"); // тут указываем ID элемента
+//             var close = $('.menu-toggle');
+//             if (close.is(e.target)) {
+        
+//             } else if (!div.is(e.target) // если клик был не по нашему блоку
+//             && div.has(e.target).length === 0) { // и не по его дочерним элементам
+//                 $('.main-menu').toggleClass('active');
+//                 $('.menu-toggle').toggleClass('active');
+//                 $('body').toggleClass('body-modal-open');
+              
+//             }
+//         });
+//     }
+
+//     //popup
+//     if(jQuery('.modal__wrap').length) {
+//         let modalWrap = $('.modal__wrap');
+//         //popup
+//         $(".modal-open").click(function (e){
+//           e.preventDefault();
+//           var btn = $(this);
+//           var numModal = btn.attr('href');
+//           if(numModal == '#modalQuiz'){
+
+//             $('.qa-del-discount').css('display', 'block');
+//           }
+//           var modal =  $(numModal);
+//         //   modalWrap.removeClass('fadeOutUp');
+//         //   modalWrap.addClass('fadeInDown');
+//           modalWrap.removeClass('animated zoomOut');
+//           modalWrap.addClass('animated zoomIn');
+//           modal.removeClass('disabled');
+//           modal.addClass('flex');
+//           $('body').addClass('body-modal-open');
+//           // body.addClass('body-modal');
+
+//         });
+//         $('.modal-close').click(function (){
+//             if ( window.innerWidth < 750 || window.screen.width < 750) {
+//                 $('.main-menu').removeClass('active');
+//                 $('.menu-toggle').removeClass('active');
+//             }
+//             // modalWrap.removeClass('fadeInDown');
+//             // modalWrap.addClass('fadeOutUp');
+//             modalWrap.removeClass('animated zoomIn');
+//             modalWrap.addClass('animated zoomOut');
+//             setTimeout(function() {
+//                 $('.modal').addClass('disabled');
+//                 }, 700);
+//             setTimeout(function() {
+//                 $('.modal').removeClass('flex');
+//                 $('body').removeClass('body-modal-open');
+//                 }, 800);  
+//         });
+//         $('.modal').mouseup(function (e){ // событие клика по веб-документу
+//           var div = $(".modal__body"); // тут указываем ID элемента
+//           var close = $('.modal-close');
+//           if (close.is(e.target)) {
+//           } else if (!div.is(e.target) // если клик был не по нашему блоку
+//           && div.has(e.target).length === 0) { // и не по его дочерним элементам
+//                 $('.main-menu').removeClass('active');
+//                 $('.menu-toggle').removeClass('active');
+//                 var modalWrap = $('.modal__wrap');
+//                 // modalWrap.removeClass('fadeInDown');
+//                 // modalWrap.addClass('fadeOutUp');
+//                 modalWrap.removeClass('animated zoomIn');
+//                 modalWrap.addClass('animated zoomOut');
+//                 setTimeout(function() {
+//                     $('.modal').addClass('disabled');
+//                 }, 700);
+//                 setTimeout(function() {
+//                     $('.modal').removeClass('flex');
+//                     $('body').removeClass('body-modal-open');
+//                 }, 800);
+//           }
+//         });
+//     }
+
+// });
+
